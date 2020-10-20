d659ff6c82806133fc6396eff2aa5975882cd63b
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-20 09:15:13 +0200

    Form for passing in upper and bottom text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+/**
+ * TODO:
+ * - put 'top' and 'bottom' in a dictionary
+ */
 function App() {
+  const [upperText, setUpperText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+
+  const handleChange = (event) => {
+    const filledName = event.target.name;
+    const fieldValue = event.target.value;
+
+    if (filledName === 'top') {
+      setUpperText(fieldValue)
+    } else {
+      setBottomText(fieldValue)
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log(upperText, bottomText);
+  }
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <form onSubmit={handleSubmit} >
+        <input type="text" name="top" value={upperText} onChange={handleChange} />
+        <input type="text" name="bottom" value={bottomText} onChange={handleChange} />
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
