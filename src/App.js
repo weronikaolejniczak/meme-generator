0f34486de451d7db4b9c75ee3d02c054b48db7b4
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-20 09:52:07 +0200

    Fetch and display generated meme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, {useState, useEffect} from 'react';
+import qs from 'qs';
 import './App.css';
 
 /**
- * TODO:
- * - put 'top' and 'bottom' in a dictionary
+ * TODO: - put 'top' and 'bottom' in a dictionary
+ * - refactor into components
+ * - change and save username and password into .env
+ * - create a gallery for meme templates
+ * - style
+ * - create routing for 'About' and 'Contact'
+ * - create header, main, footer
  */
+
 function App() {
   const [upperText, setUpperText] = useState('');
   const [bottomText, setBottomText] = useState('');
   const [templates, setTemplates] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const [error, setError] = useState(null);
+  const [meme, setMeme] = useState(null);
 
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
@@ -27,22 +37,51 @@ function App() {
     }
   }
 
+  const handleClick = (imageId) => {
+    setSelectedId(imageId);
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(templates);
-    //console.log(upperText, bottomText);
+    setError(null);
+    const params = {
+      template_id: selectedId,
+      text0: upperText,
+      text1: bottomText,
+      username: 'xzk03017',
+      password: 'xzk03017@cndps.com'
+    }
+    fetch(`https://api.imgflip.com/caption_image?${qs.stringify(params)}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.success) {
+          setMeme(json.data);
+        } else {
+          setError(json.error_message);
+        }
+      })
   }
 
   return (
     <div className="App">
+      {error}
+      Selected meme template: {selectedId}
       <form onSubmit={handleSubmit} >
         <input type="text" name="top" value={upperText} onChange={handleChange} />
         <input type="text" name="bottom" value={bottomText} onChange={handleChange} />
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Generate meme"/>
       </form>
+      <div className="meme">
+        {meme && (<img src={meme.url} alt="Generated meme" />)}
+      </div>
       <div className="meme-container">
         {templates && templates.map((elem) => (
-          <img key={elem.id} src={elem.url} alt="" />
+          <img
+            key={elem.id}
+            onClick={() => handleClick(elem.id)}
+            src={elem.url}
+            alt={elem.name}
+          />
         ))}
       </div>
     </div>
