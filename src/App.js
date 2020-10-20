80dd37895e10702edef7e35da1544ce7dff36831
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-10-20 09:22:42 +0200

    Fetch meme images and render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 /**
@@ -8,6 +8,13 @@ import './App.css';
 function App() {
   const [upperText, setUpperText] = useState('');
   const [bottomText, setBottomText] = useState('');
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => res.json())
+      .then(json => setTemplates(json.data.memes))
+  }, []);
 
   const handleChange = (event) => {
     const filledName = event.target.name;
@@ -22,6 +29,7 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(templates);
     //console.log(upperText, bottomText);
   }
 
@@ -32,6 +40,11 @@ function App() {
         <input type="text" name="bottom" value={bottomText} onChange={handleChange} />
         <input type="submit" value="Submit"/>
       </form>
+      <div className="meme-container">
+        {templates && templates.map((elem) => (
+          <img key={elem.id} src={elem.url} alt="" />
+        ))}
+      </div>
     </div>
   );
 }
